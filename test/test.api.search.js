b9dfb61bffb3b9ed15268634471610bd1bf47563
ececilla <ececilla@server>
2013-03-04 19:36:18 +0100

    search tests are old list tests.

--- test/test.api.search.js
+++ test/test.api.search.js
@@ -1,6 +1,6 @@
 var sandbox = require("sandboxed-module");
 
-exports["api.remote.list: missing & wrong params"] = function(test){
+exports["api.remote.search: missing & wrong params"] = function(test){
 
 	var dbdocs = {};
 	dbdocs["50187f71556efcbb25000002"] = {_id:"50187f71556efcbb25000002",uid:620793114, ctime:1350094951092, keywords:["test1","test2","test3"], z:{y:1}, rcpts:[620793114,620793115]};
@@ -11,21 +11,21 @@ exports["api.remote.list: missing & wrong params"] = function(test){
 	
 	var sb = sandbox.require("../lib/sandbox",{
 		requires:{
-					"./server":{config:{app:{status:1},db:{default_catalog:"docs", system_catalogs:["timers","events"], user_catalogs:["docs","dummy"]}},api:{config:{procedures:{list:1}}}} 
+					"./server":{config:{app:{status:1},db:{default_catalog:"docs", system_catalogs:["timers","events"], user_catalogs:["docs","dummy"]}},api:{config:{procedures:{search:1}}}} 
 		}
 	});
 	
 	sb.init();
-	sb.add_constraint_pre("list","not_catalog",sb.constraints.not_catalog,"timers")
-	  .add_constraint_pre("list","not_catalog",sb.constraints.not_catalog,"events")	
-	  .add_constraint_pre("list","user_catalog",sb.constraints.user_catalog)
-	  .add_constraint_pre("list","param_uid",sb.constraints.is_required("uid"))
-	  .add_constraint_pre("list","param_criteria",sb.constraints.is_required("criteria"));	  	  	  	    	  
+	sb.add_constraint_pre("search","not_catalog",sb.constraints.not_catalog,"timers")
+	  .add_constraint_pre("search","not_catalog",sb.constraints.not_catalog,"events")	
+	  .add_constraint_pre("search","user_catalog",sb.constraints.user_catalog)
+	  .add_constraint_pre("search","param_uid",sb.constraints.is_required("uid"))
+	  .add_constraint_pre("search","param_criteria",sb.constraints.is_required("criteria"));	  	  	  	    	  
 	  
 	  	  							
 	//user catalog
 	var params = {catalog:"timers", uid:"50187f71556efcbb25aaaa", keyword:"test1"};
-	sb.execute("list", params, function(err,result){
+	sb.execute("search", params, function(err,result){
 					
 		test.deepEqual(err, {code:-5, message:"No access permission: restricted catalog"});
 				
@@ -33,7 +33,7 @@ exports["api.remote.list: missing & wrong params"] = function(test){
 	
 	//uid missing: must be an existing system user.
 	params = {miss_uid:"50187f71556efcbb25aaaa", criteria:"test1"};
-	sb.execute("list", params, function(err,result){
+	sb.execute("search", params, function(err,result){
 					
 		test.deepEqual(err, {code:-12, message: "uid parameter required"});
 				
@@ -42,7 +42,7 @@ exports["api.remote.list: missing & wrong params"] = function(test){
 			
 	//criteria missing
 	params = {uid:"50187f71556efcbb25aaaa", miss_criteria:"test1"};
-	sb.execute("list", params, function(err,result){
+	sb.execute("search", params, function(err,result){
 					
 		test.deepEqual(err, {code:-12, message: "criteria parameter required"});		
 		test.done();	
@@ -52,7 +52,7 @@ exports["api.remote.list: missing & wrong params"] = function(test){
 }
 
 
-exports["api.remote.list: valid params with results"] = function(test){
+exports["api.remote.search: valid params with results"] = function(test){
 
 	var dbdocs = {};
 	dbdocs["50187f71556efcbb25000003"] = {_id:"50187f71556efcbb25000003",uid:620793114, ctime:1350094951092, name:"enric", z:{y:1}, rcpts:[620793114,620793115]};
@@ -72,7 +72,7 @@ exports["api.remote.list: valid params with results"] = function(test){
 								ret_handler(null,[dbdocs["50187f71556efcbb25000003"],dbdocs["50187f71556efcbb25000002"]]);//return both documents									
 							}
 						 },
-					"./server":{config:{app:{status:1},db:{default_catalog:"docs", system_catalogs:["timers","events"], user_catalogs:["docs","dummy"]}},api:{config:{procedures:{list:1}}}} 
+					"./server":{config:{app:{status:1},db:{default_catalog:"docs", system_catalogs:["timers","events"], user_catalogs:["docs","dummy"]}},api:{config:{procedures:{search:1}}}} 
 		}
 	});
 	
@@ -90,20 +90,20 @@ exports["api.remote.list: valid params with results"] = function(test){
 							}
 						 },
 					"./api":api,	 
-					"./server":{config:{app:{status:1},db:{default_catalog:"docs", system_catalogs:["timers","events"], user_catalogs:["docs","dummy"]}},api:{config:{procedures:{list:1}}}} 
+					"./server":{config:{app:{status:1},db:{default_catalog:"docs", system_catalogs:["timers","events"], user_catalogs:["docs","dummy"]}},api:{config:{procedures:{search:1}}}} 
 		}
 	});
 	
 	sb.init();
-	sb.add_constraint_pre("list","not_catalog",sb.constraints.not_catalog,"timers")
-	  .add_constraint_pre("list","not_catalog",sb.constraints.not_catalog,"events")	
-	  .add_constraint_pre("list","user_catalog",sb.constraints.user_catalog)
-	  .add_constraint_pre("list","param_uid",sb.constraints.is_required("uid"))
-	  .add_constraint_pre("list","param_criteria",sb.constraints.is_required("criteria"));	  	  	  	    	  
+	sb.add_constraint_pre("search","not_catalog",sb.constraints.not_catalog,"timers")
+	  .add_constraint_pre("search","not_catalog",sb.constraints.not_catalog,"events")	
+	  .add_constraint_pre("search","user_catalog",sb.constraints.user_catalog)
+	  .add_constraint_pre("search","param_uid",sb.constraints.is_required("uid"))
+	  .add_constraint_pre("search","param_criteria",sb.constraints.is_required("criteria"));	  	  	  	    	  
 	  
 	  	  								
 	var params = {uid:"50187f71556efcbb25aaaa", criteria:{name:"enric"}, catalog:"docs"};
-	sb.execute("list", params, function(err,ctx){
+	sb.execute("search", params, function(err,ctx){
 					
 		test.equal(err, undefined);
 		test.deepEqual(ctx.retval,[{wid:"50187f71556efcbb25000003",uid:620793114, ctime:1350094951092, name:"enric", z:{y:1}, rcpts:[620793114,620793115]},{wid:"50187f71556efcbb25000002",uid:620793114, ctime:1350094951092, name:"enric", z:{y:1}, rcpts:[620793114,620793115]}])		
@@ -116,7 +116,7 @@ exports["api.remote.list: valid params with results"] = function(test){
 	
 }
 
-exports["api.remote.list: valid params with no results"] = function(test){
+exports["api.remote.search: valid params with no results"] = function(test){
 
 	var dbdocs = {};
 	dbdocs["50187f71556efcbb25000003"] = {_id:"50187f71556efcbb25000003",uid:620793114, ctime:1350094951092, name:"enric", z:{y:1}, rcpts:[620793114,620793115]};
@@ -136,7 +136,7 @@ exports["api.remote.list: valid params with no results"] = function(test){
 								ret_handler(null,[]);//return both documents									
 							}
 						 },
-					"./server":{config:{app:{status:1},db:{default_catalog:"docs", system_catalogs:["timers","events"], user_catalogs:["docs","dummy"]}},api:{config:{procedures:{list:1}}}} 
+					"./server":{config:{app:{status:1},db:{default_catalog:"docs", system_catalogs:["timers","events"], user_catalogs:["docs","dummy"]}},api:{config:{procedures:{search:1}}}} 
 		}
 	});
 	
@@ -154,20 +154,20 @@ exports["api.remote.list: valid params with no results"] = function(test){
 							}
 						 },
 					"./api":api,	 
-					"./server":{config:{app:{status:1},db:{default_catalog:"docs", system_catalogs:["timers","events"], user_catalogs:["docs","dummy"]}},api:{config:{procedures:{list:1}}}} 
+					"./server":{config:{app:{status:1},db:{default_catalog:"docs", system_catalogs:["timers","events"], user_catalogs:["docs","dummy"]}},api:{config:{procedures:{search:1}}}} 
 		}
 	});
 	
 	sb.init();
-	sb.add_constraint_pre("list","not_catalog",sb.constraints.not_catalog,"timers")
-	  .add_constraint_pre("list","not_catalog",sb.constraints.not_catalog,"events")	
-	  .add_constraint_pre("list","user_catalog",sb.constraints.user_catalog)
-	  .add_constraint_pre("list","param_uid",sb.constraints.is_required("uid"))
-	  .add_constraint_pre("list","param_criteria",sb.constraints.is_required("criteria"));	  	  	  	    	  
+	sb.add_constraint_pre("search","not_catalog",sb.constraints.not_catalog,"timers")
+	  .add_constraint_pre("search","not_catalog",sb.constraints.not_catalog,"events")	
+	  .add_constraint_pre("search","user_catalog",sb.constraints.user_catalog)
+	  .add_constraint_pre("search","param_uid",sb.constraints.is_required("uid"))
+	  .add_constraint_pre("search","param_criteria",sb.constraints.is_required("criteria"));	  	  	  	    	  
 	  
 	  	  								
 	var params = {uid:"50187f71556efcbb25aaaa", criteria:{name:"baaar"}, catalog:"docs"};
-	sb.execute("list", params, function(err,ctx){
+	sb.execute("search", params, function(err,ctx){
 					
 		test.equal(err, undefined);
 		test.deepEqual(ctx.retval,[]);		
