77dbd3f38cd10fed5d4ce7bb1b9574f717219018
ececilla <ececilla@server>
2013-03-20 13:06:20 +0100

    test refactored because module changed behaviour.

--- test/test.timers.js
+++ test/test.timers.js
@@ -1,11 +1,14 @@
 var sandbox = require("sandboxed-module");
+var time = require("../lib/time");
 
 exports["module exported functions"] = function(test){
 	
 	var timers = require("../lib/timers");
 	
 	test.notEqual( timers.add_timer_type, undefined );
-	test.notEqual( timers.start_timer, undefined );	
+	test.notEqual( timers.start_timer, undefined );
+	test.notEqual( timers.remove_timer, undefined );
+	test.notEqual( timers.execute_timer, undefined );	
 	test.notEqual( timers.load, undefined );
 	test.done();
 	
@@ -48,10 +51,32 @@ exports["timers.start_timer"] = function(test){
 	
 }
 
-exports["timers.load"] = function(test){
+exports["timers.load:empty"] = function(test){
+				
+	
+	var timers = sandbox.require("../lib/timers",{
+		requires:{"./db":{
+							criteria:function( col_str, criteria, order_field, ret_handler ){
+								
+									
+									test.equal(col_str,"timers");
+									test.deepEqual(criteria,{});	
+									ret_handler(null,[]);								
+							}
+				}		
+		}
+	});
+		
+	
+	timers.load();
+	test.expect(2);
+	test.done();
+}
+
+exports["timers.load:1 future timer"] = function(test){
 	
 		
-	var arr_timers = [{type_name:"dummy",etime:1379699594550,data:{catalog:"docs"}},{type_name:"dummy",etime:1379699595550,data:{catalog:"docs"}},{type_name:"dummy",etime:1379699598550,data:{catalog:"docs"}}];
+	var arr_timers = [{_id:"hh879384",type_name:"dummy",etime:time.now()+3000,data:{foo:"bar"}}];
 	
 	var timers = sandbox.require("../lib/timers",{
 		requires:{"./db":{
@@ -61,20 +86,62 @@ exports["timers.load"] = function(test){
 									test.equal(col_str,"timers");
 									test.deepEqual(criteria,{});	
 									ret_handler(null,arr_timers);								
+							},
+							
+							removeById:function(col_str, id_str, ret_handler){
+								
+								test.equal(col_str,"timers");
+								test.equal(id_str,"hh879384");
+								test.expect(5);
+								test.done();	
+								ret_handler(null);
 							}
 				}		
 		}
 	});
-	
-	//this function will be called from load
-	var ncall = 0;
-	timers.start_timer = function( timer_data){
-				
-		test.deepEqual(timer_data,arr_timers[ncall++]);
-								
-	}
-	
+		
+	timers.add_timer_type("dummy",function(timer){
+		
+		test.deepEqual(timer,{foo:"bar"});
+		
+	});
 	timers.load();
-	test.expect(5);
-	test.done();
+	
 }
+
+exports["timers.load:1 past timer"] = function(test){
+	
+		
+	var arr_timers = [{_id:"hh879384",type_name:"dummy",etime:time.now()-1000,data:{foo:"bar"}}];
+	
+	var timers = sandbox.require("../lib/timers",{
+		requires:{"./db":{
+							criteria:function( col_str, criteria, order_field, ret_handler ){
+								
+									
+									test.equal(col_str,"timers");
+									test.deepEqual(criteria,{});	
+									ret_handler(null,arr_timers);								
+							},
+							
+							removeById:function(col_str, id_str, ret_handler){
+								
+								test.equal(col_str,"timers");
+								test.equal(id_str,"hh879384");
+								test.expect(5);
+								test.done();	
+								ret_handler(null);
+							}
+				}		
+		}
+	});
+		
+	timers.add_timer_type("dummy",function(timer){
+		
+		test.deepEqual(timer,{foo:"bar"});
+		
+	});
+	timers.load();
+	
+}
+
