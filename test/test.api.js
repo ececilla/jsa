14df6fcd091645ef0a27c38a4cd01114d7c78cc5
ececilla <ececilla@server>
2013-03-27 01:29:44 +0100

    tag feature tests added.

--- test/test.api.js
+++ test/test.api.js
@@ -59,10 +59,55 @@ exports["api.emit:params, explicit rcpts"] = function(test){
 		test.equal(typeof msg.ev_tstamp, "number");
 		test.deepEqual(msg.ev_ctx, ctx);
 		test.deepEqual(rcpts, emit_rcpts);
+		test.equal(msg.ev_tag,undefined);
+		test.expect(6);
 		test.done();
 	});
 	
 	api.emit("ev_foo", ctx, emit_rcpts);
+				
+}
+
+exports["api.emit:params, explicit rcpts, tag"] = function(test){
+	
+	var api = require("../lib/api");			
+	var ctx = {params:{foo:1, bar:5}, doc:undefined};
+	var emit_rcpts = [1,2,3];
+	
+	api.on("ev_foo2", function(msg, rcpts){
+		
+		test.equal(msg.ev_type, "ev_foo2");
+		test.notEqual(msg.ev_tstamp, undefined);
+		test.equal(typeof msg.ev_tstamp, "number");
+		test.deepEqual(msg.ev_ctx, ctx);
+		test.deepEqual(rcpts, emit_rcpts);
+		test.equal(msg.ev_tag,"tag1");
+		test.expect(6);
+		test.done();
+	});
+	
+	api.emit("ev_foo2", ctx, emit_rcpts, "tag1");
+				
+}
+
+exports["api.emit:params, tag"] = function(test){
+	
+	var api = require("../lib/api");			
+	var ctx = {params:{foo:1, bar:5}, doc:undefined};	
+	
+	api.on("ev_foo3", function(msg, rcpts){
+		
+		test.equal(msg.ev_type, "ev_foo3");
+		test.notEqual(msg.ev_tstamp, undefined);
+		test.equal(typeof msg.ev_tstamp, "number");
+		test.deepEqual(msg.ev_ctx, ctx);
+		test.equal(rcpts, undefined);
+		test.equal(msg.ev_tag,"tag1");
+		test.expect(6);
+		test.done();
+	});
+	
+	api.emit("ev_foo3", ctx, "tag1");
 				
 }
 
